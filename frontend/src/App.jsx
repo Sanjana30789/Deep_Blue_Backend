b1dc92a8f8b4f5d05e92fbc654a9318ef2d511d2
Sanjana30789 <sanjanachoubey763@gmail.com>
2025-02-13 10:09:46 +0530

    Changes to UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
 import Dashboard from "./Components/DataDisplay";
-import PieChartPage from './Components/Piechart'
+import PieChartPage from './Components/Piechart';
+import LandingPage from './Components/landing';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/piechart" element={<PieChartPage />} />
